15987c4805c3c10d6e7d2340dc5c355a1ff9c22e
Kazunari Hirosawa <9694038+Kazunari-h@users.noreply.github.com>
2026-06-23 13:17:30 +0900

    Initial Commit

---
+++ cafesns/cafesnsApp.swift
@@ -0,0 +1,17 @@
+//
+//  cafesnsApp.swift
+//  cafesns
+//
+//  Created by Kazunari Hirosawa on 2026/06/23.
+//
+
+import SwiftUI
+
+@main
+struct cafesnsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
